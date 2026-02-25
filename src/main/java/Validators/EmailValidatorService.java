82b107eabb9ef68f5ed865194a53199800cf2af7
CarlosIbarraMora <160541756+CarlosIbarraMora@users.noreply.github.com>
2026-02-25 10:14:28 -0800

    Custom email validator and test passed

--- src/main/java/Validators/EmailValidatorService.java
+++ src/main/java/Validators/EmailValidatorService.java
@@ -1,7 +1,65 @@
 package Validators;
 
 public class EmailValidatorService {
+
     public boolean isValid(String email) {
-        return email != null && !email.isEmpty();
+
+        if (email == null || email.isEmpty()) return false;
+
+
+        if (!validSeparation(email)) return false;
+
+        String[] parts = email.split("#");
+        if(parts.length > 2 || parts.length < 1)return false;
+
+        String user = parts[0];
+        String domain = parts[1];
+
+        if (!validContains4(email)) return false;
+        if (!validEmailLength(email)) return false;
+        if (!validDomainLength(domain)) return false;
+        if (!validUserCharacters(user)) return false;
+        if (!validDomainCharacters(domain)) return false;
+        if (!validDiphthong(email)) return false;
+
+        return true;
+    }
+
+    //checks if email length no longer than 47
+    private boolean validEmailLength(String email) {
+        return email.length() <= 47;
+    }
+
+    //checks if domain length no longer than 5
+    private boolean validDomainLength(String domain) {
+        String[] providerDomain = domain.split("\\.");
+        if (providerDomain.length < 2) return false;
+        return providerDomain[1].length() <= 5;
+    }
+
+    //Checks if user part of email only contains: a-z 0-9.+_-
+    private boolean validUserCharacters(String user) {
+        return user.matches("^[a-z0-9.+_-]+$");
+    }
+
+    //Checks if domain part of email only contains: a-z, 0-9.
+    private boolean validDomainCharacters(String domain) {
+        return domain.matches("^[a-z0-9.]+$");
+    }
+
+    //Checks if in the whole email it doesn't contain one vowel after another
+    private boolean validDiphthong(String user) {
+        return !user.matches(".*[aeiou]{2}.*");
+    }
+
+    //checks if email is separated by #
+    private boolean validSeparation(String email) {
+        String[] temp = email.split("#");
+        return temp.length == 2;
+    }
+
+    //checks if 4 is present in the email
+    private boolean validContains4(String email) {
+        return email.contains("4");
     }
 }
--- src/test/java/mx/edu/cetys/validators/EmailValidatorServiceTest.java
+++ src/test/java/mx/edu/cetys/validators/EmailValidatorServiceTest.java
@@ -7,6 +7,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmailValidatorServiceTest {
+
+    //Base case: valid email
+    @Test
+    void shouldReturnTrueWhenEmailIsValid() {
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos4gmal#.com");
+
+        //Assert
+        assertTrue(isValid);
+    }
+
     @Test
     void shouldReturnFalseWhenEmailIsNull(){
         //Arrange
@@ -24,6 +37,89 @@ public class EmailValidatorServiceTest {
         EmailValidatorService emailValidator = new EmailValidatorService();
         //Act
         var isValid = emailValidator.isValid("");
+        //Assert
+        assertFalse(isValid);
+    }
+    // Characters allowed 1-0, a-z
+    @Test
+    void shouldReturnFalseWhenInvalidCharactersGeneral(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos4&#gmal.com");
+        //Assert
+        assertFalse(isValid);
+    }
+    //Special characters allowed user: .-_+
+    @Test
+    void shouldReturnFalseWhenInvalidUserCharacters(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos4!#gmal.com");
+        //Assert
+        assertFalse(isValid);
+    }
+    //Special characters allowed(provider, domain): .
+    @Test
+    void shouldReturnFalseWhenDomainCharactersAreInvalid(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos4#gmal^.com");
+        //Assert
+        assertFalse(isValid);
+    }
+    //Use # to separate user-provider
+    @Test
+    void shouldReturnFalseWhenSeparationCharacterIsInvalid(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos4@gmal.com");
+        //Assert
+        assertFalse(isValid);
+    }
+    //No diphthong in the whole email
+    @Test
+    void shouldReturnFalseWhenDiphthongIsPresent(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos4#gmail.com");
+        //Assert
+        assertFalse(isValid);
+    }
+    // Max Domain length: 5
+    @Test
+    void shouldReturnFalseWhenDomainLengthHigherThan5(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos4#gmal.comasi");
+
+        //Assert
+        assertFalse(isValid);
+    }
+    //Max email length: 47
+    @Test
+    void shouldReturnFalseWhenEmailLengthHigherThan47(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("cccccccccccccccccccccccccccccccccccccccccccccccccccccccarlos4#gmal.com");
+
+        //Assert
+        assertFalse(isValid);
+    }
+    //Mandatory, include 4 in email
+    @Test
+    void shouldReturnFalseWhen4IsNotPresent(){
+        //Arrange
+        EmailValidatorService emailValidator = new EmailValidatorService();
+        //Act
+        var isValid = emailValidator.isValid("carlos#gmal.com");
+
         //Assert
         assertFalse(isValid);
     }
